e7cf12bc2d225f41ebb9ce3e992289aa142dcaba
walbyn <walbyngonzalez@gmail.com>
2025-09-19 14:29:25 -0600

    Se agrega los archivos

---
+++ miproyecto.php
@@ -0,0 +1,6 @@
+<?php
+// Mostramos la fecha y hora actuales con PHP
+date_default_timezone_set('America/Costa_Rica'); // Se ajusta la fecha
+echo "<h1>Fecha y hora actual </h1>";
+echo "<p>" . date("Y-m-d H:i:s") . "</p>";
+?>
